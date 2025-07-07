f334363fa9f4da608e78520884bfe2826162909f
AshveryaSinghal <142670892+AshveryaSinghal@users.noreply.github.com>
2025-07-07 19:36:15 +0530

    Update AIMouse.py

--- AIMouse.py
+++ AIMouse.py
@@ -5,12 +5,11 @@ import Handtracking as htm
 import time
 import autopy
 
-##########################
+
 wCam, hCam = 640, 480
-frameR = 100  # Frame Reduction
+frameR = 100  
 smoothening = 7
-click_distance = 35  # Distance between fingers to trigger click
-#########################
+click_distance = 35 
 
 pTime = 0
 plocX, plocY = 0, 
@@ -27,7 +26,7 @@ detector = htm.handDetector(maxHands=1)
 wScr, hScr = autopy.screen.size()
 
 while True:
-    # 1. Find hand Landmarks
+   
     success, img = cap.read()
     if not success:
         print("Failed to capture image")
@@ -37,27 +36,26 @@ while True:
     lmList = detector.findPosition(img)
 
     if len(lmList) != 0:
-        # 2. Get the tip positions of index and thumb
-        x1, y1 = lmList[8][1:]  # Index finger
-        x2, y2 = lmList[4][1:]  # Thumb
+       
+        x1, y1 = lmList[8][1:]  
+        x2, y2 = lmList[4][1:] 
 
-        # 3. Check which fingers are up
+       
         fingers = detector.fingersUp()
 
         cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
                       (255, 0, 255), 2)
 
-        # 4. Only Index Finger : Moving Mode
         if fingers[1] == 1 and fingers[2] == 0:
-            # 5. Convert Coordinates
+          
             x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
             y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
 
-            # 6. Smoothen Values
+         
             clocX = plocX + (x3 - plocX) / smoothening
             clocY = plocY + (y3 - plocY) / smoothening
 
-            # 7. Move Mouse
+          
             try:
                 autopy.mouse.move(wScr - clocX, clocY)
                 cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
@@ -65,7 +63,7 @@ while True:
             except:
                 pass
 
-        # 8. Click when thumb and index finger are close (pinch gesture)
+      
         length, img, lineInfo = detector.findDistance(8, 4, img)
         if length < click_distance:
             cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
@@ -74,14 +72,12 @@ while True:
             except:
                 pass
 
-    # 9. Frame Rate
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
     cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,
                 (255, 0, 0), 3)
 
-    # 10. Display
     cv2.imshow("AI Mouse Control", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
